e34e11af14225f46dd8efcb2d5e9603bc030f782
Jajaichbin <does@not.matter>
2025-12-26 16:17:03 +0100

    Cleaner version

--- rust_sniffer/src/main.rs
+++ rust_sniffer/src/main.rs
@@ -1,27 +1,16 @@
-use bindings::{pcap_if_t, pcap_findalldevs_ex, PCAP_SRC_IF_STRING, PCAP_ERRBUF_SIZE, pcap_remoteact_cleanup as aaa};
+use bindings::{pcap_if_t, pcap_findalldevs_ex, PCAP_SRC_IF_STRING, pcap_freealldevs };
 
-use std::ptr::null_mut;
+use std::ffi::CStr;
 
 // println!("MIDDLE");
 
-fn main() {
-    println!("NOT EVEN HERE");
-    // let mut _alldevs: *mut pcap_if_t = null_mut();
-    // let alldevs = &raw mut _alldevs;
 
-    // let mut _s = String::with_capacity(256);
+fn print_adapters() {
     let mut a = [0 as ::std::os::raw::c_char; 256];
-    let mut errbuf:*mut ::std::os::raw::c_char = a.as_mut_ptr();
+    let errbuf:*mut ::std::os::raw::c_char = a.as_mut_ptr(); // mut???
 
-    println!("MIDDLE");
-
-    // a[2] = 127 as ::std::os::raw::c_char;
-
-    // println!("{:?}", unsafe {*errbuf.add(2)});
     
     let pcap_src_if_string_as_ptr: *const i8 = PCAP_SRC_IF_STRING.as_ptr() as *const i8;
-
-    println!("STR is -{:?}-", PCAP_SRC_IF_STRING);
 
     let mut alldevs: *mut pcap_if_t = std::ptr::null_mut();
 
@@ -30,21 +19,35 @@ fn main() {
             pcap_src_if_string_as_ptr,
             std::ptr::null_mut(),
             &mut alldevs,
-            errbuf,
+            errbuf, // TODO: not sure whether anything can be actually written to this buffer.
         )
     };
-    println!("Result is -{}-", findalldevs_out);
-    // unsafe {pcap_remoteact_cleanup();}
-    // println!("{:p}", aaa as *const ());
 
-    // unsafe {
-    //     aaa();
-    // }
+    if findalldevs_out == -1 {
+        println!("Error in pcap_findalldevs_ex: {:?}", unsafe {CStr::from_ptr(errbuf)});
+        panic!();
+    }
 
-    println!("DONE");
+    let mut current_dev_p = alldevs;
+    let mut i = 0;
+    while !current_dev_p.is_null() {
+        let current_dev = unsafe{ *current_dev_p};
+        println!("{} - {:?}", i, unsafe { CStr::from_ptr(current_dev.description)});
 
-    // if findalldevs_out == -1 {
-    //     println!("Error in pcap_findalldevs_ex: ");
-    //     // exit(1);
-    // }
+        current_dev_p = current_dev.next; 
+        i += 1;
+    }
+
+
+    unsafe {
+        pcap_freealldevs(alldevs);
+    }
+}
+
+fn main() {
+    println!("Started");
+
+    print_adapters();
+
+    println!("Finished");
 }
